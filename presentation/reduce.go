adb4d9e7260fdbc658f3fdafc09955f2d880cdeb
geometricgrouptheorydev <geometricgrouptheorydev@gmail.com>
2026-01-11 08:52:33 +0300

    chore: word reduction methods setup

---
+++ presentation/reduce.go
@@ -0,0 +1,55 @@
+package presentation
+
+//In this file, we'll have the word reduction methods based on the different group presenetation classes
+//reductions ordered in levels of power
+
+//the higher the priority the better the reduction algorithm for computation!
+var reduceCLassPriority = []Class{Trivial, Cyclic, Abelian, Free, OneRelator}
+
+var reduceHandles = map[Class]func(GroupPresentation, Word) Word{
+	Trivial: GroupPresentation.handleReduceTrivial,
+	Cyclic: GroupPresentation.handleReduceCyclic,
+	Abelian: GroupPresentation.handleReduceAbelian,
+	Free: GroupPresentation.handleReduceFree,
+	OneRelator: GroupPresentation.handleReduceOneRelator,
+}
+
+func (G GroupPresentation) Reduce(w Word) Word {
+	for _, c := range reduceCLassPriority {
+		if val, ok := G.classes[c]; val && ok {
+			switch c{
+			case Trivial:
+				return G.handleReduceTrivial(w)
+			case Cyclic:
+				return G.handleReduceCyclic(w)
+			case Abelian:
+				return G.handleReduceAbelian(w)
+			case Free:
+				return G.handleReduceFree(w)
+			case OneRelator:
+				return G.handleReduceOneRelator(w)
+			}
+		}
+	}
+	return Word{}
+}
+
+func (G GroupPresentation) handleReduceTrivial(w Word) Word{
+	return Word{}
+}
+
+func (G GroupPresentation) handleReduceCyclic(w Word) Word{
+	return Word{}
+}
+
+func (G GroupPresentation) handleReduceAbelian(w Word) Word{
+	return Word{}
+}
+
+func (G GroupPresentation) handleReduceFree(w Word) Word {
+	return Word{}
+}
+
+func (G GroupPresentation) handleReduceOneRelator(w Word) Word{
+	return Word{}
+}
